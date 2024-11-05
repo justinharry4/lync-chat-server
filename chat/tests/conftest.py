4983d39628d42a47bbcf9af00d291d490b9d1e37
JUSTINHARRY <justinharryjayd@gmail.com>
2024-11-05 02:57:02 +0100

    add tests for private chat list and retrieve endpoints
    - add tests for `GET /chat/privatechats/pk/` endpoint
    - add tests for `GET /chat/privatechats/` endpoint
    - modify existing fixtures and add new ones to enhance
      code readablity

--- chat/tests/conftest.py
+++ chat/tests/conftest.py
@@ -2,21 +2,25 @@ from rest_framework.test import APIClient
 from pytest import fixture
 from model_bakery import baker
 
-from core.models import User
 from chat.models import PrivateChat, PrivateChatParticipant
 
+
+class Constants:
+    INVALID_ID = 1000
+
+@fixture
+def test_data():
+    return Constants()
 
 @fixture
 def api_client():
     return APIClient()
-
 
 @fixture
 def authenticate(api_client):
     def do_authenticate(user):
         api_client.force_authenticate(user=user)
     return do_authenticate
-
 
 @fixture
 def create_private_chat():
@@ -30,3 +34,15 @@ def create_private_chat():
         )
         return private_chat
     return do_create_private_chat
+
+@fixture
+def create_private_chats_for_user(create_private_chat):
+    def do_create_private_chats(user, other_users):
+        private_chats = []
+        for u in other_users:
+            private_chat = create_private_chat([user, u])
+            private_chats.append(private_chat)
+        return private_chats
+    return do_create_private_chats
+
+
--- chat/tests/test_private_chats.py
+++ chat/tests/test_private_chats.py
@@ -18,6 +18,23 @@ def call_retrieve_pc_endpoint(api_client):
         return api_client.get(f'/chat/privatechats/{pk}/')
     return make_api_call
 
+@pytest.fixture
+def call_list_pcs_endpoint(api_client):
+    def make_api_call():
+        return api_client.get(f'/chat/privatechats/')
+    return make_api_call
+
+@pytest.fixture
+def is_member_of_private_chats():
+    def check_membership(user, private_chats):
+        for private_chat in private_chats:
+            target = [p for p in private_chat['participants']
+                      if p['user'] == user.id]
+            if len(target) == 0:
+                return False
+        return True
+    return check_membership
+
 
 @pytest.mark.django_db
 class TestCreatePrivateChat():
@@ -48,11 +65,16 @@ class TestCreatePrivateChat():
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['participant_user_ids'] is not None
 
-    def test_if_invalid_ids_returns_400(self, authenticate, call_create_pc_endpoint):
+    def test_if_invalid_ids_returns_400(self,
+                                        test_data,
+                                        authenticate,
+                                        call_create_pc_endpoint):
         user = baker.make(User)
         authenticate(user)
 
-        response = call_create_pc_endpoint({'participant_user_ids': [user.id, 1000]})
+        response = call_create_pc_endpoint({
+            'participant_user_ids': [user.id, test_data.INVALID_ID]
+        })
         
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['participant_user_ids'] is not None
@@ -79,8 +101,68 @@ class TestCreatePrivateChat():
         assert response.data['id'] > 0
 
 
+@pytest.mark.django_db
 class TestRetrievePrivateChat():
     def test_if_user_is_anonymous_returns_401(self, call_retrieve_pc_endpoint):
         response = call_retrieve_pc_endpoint(1)
         
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_member_returns_403(self,
+                                               authenticate,
+                                               create_private_chat,
+                                               call_retrieve_pc_endpoint):
+        users = baker.make(User, _quantity=3)
+        private_chat = create_private_chat(users[:2])
+        authenticate(users[2])
+
+        response = call_retrieve_pc_endpoint(private_chat.id)
+        
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_if_id_does_not_exist_returns_404(self,
+                                              authenticate,
+                                              test_data,
+                                              call_retrieve_pc_endpoint):
+        user = baker.make(User)
+        authenticate(user)
+
+        response = call_retrieve_pc_endpoint(test_data.INVALID_ID)
+        
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_if_user_is_a_member_returns_200(self,
+                                             authenticate,
+                                             create_private_chat,
+                                             call_retrieve_pc_endpoint):
+        users = baker.make(User, _quantity=2)
+        private_chat = create_private_chat(users)
+        authenticate(users[0])
+
+        response = call_retrieve_pc_endpoint(private_chat.id)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['id'] == private_chat.id
+
+
+@pytest.mark.django_db
+class TestListPrivateChats():
+    def test_if_user_is_anonymous_returns_401(self, call_list_pcs_endpoint):
+        response = call_list_pcs_endpoint()
+        
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_authenticated_returns_200(self,
+                                                  authenticate,
+                                                  call_list_pcs_endpoint,
+                                                  create_private_chats_for_user,
+                                                  is_member_of_private_chats):
+        users = baker.make(User, _quantity=10)
+        create_private_chats_for_user(users[0], users[1:5])
+        create_private_chats_for_user(users[5], users[6:])
+        authenticate(users[0])
+
+        response = call_list_pcs_endpoint()
+        
+        assert response.status_code == status.HTTP_200_OK
+        assert is_member_of_private_chats(users[0], response.data)
